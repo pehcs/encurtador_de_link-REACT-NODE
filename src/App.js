7c2f9bc19f28c50796cb502a43d738d311356812
Pedro Correia <pehcorreias@gmail.com>
2022-01-13 21:52:48 -0300

    Subindo App REACT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Guys!</h1>
+      <p><b>HELLO WORLD</b> pra não perder o costume</p>
+    </div>
+  );
+}
+
+export default App;
