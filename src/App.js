188db65bfefcfca42c09a7ffc5befe68ff5b392a
Ayan Faustt <ayanfaustt@gmail.com>
2022-01-17 16:02:43 -0300

    fix: correcao do nome dos arquivos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import Footer from './partials/Footer';
 
 function App() {
   return (
     <div>
       <h1>Hello Guys!</h1>
       <p><b>HELLO WORLD</b> pra não perder o costume</p>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/partials/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Footer(){
+    return(
+        <h1>
+            Teste Caraio
+        </h1>
+    )
+}
+
+
+export default Footer;
