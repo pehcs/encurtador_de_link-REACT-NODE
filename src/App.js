b42862979ddf827a43713c6209a8cbedd4a6aa8c
Pedro Correia <pehcorreias@gmail.com>
2022-01-17 14:09:57 -0300

    pasta partials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import AdvancedStatistics from './partials/Main'
 
 function App() {
   return (
     <div>
       <h1>Hello Guys!</h1>
       <p><b>HELLO WORLD</b> pra não perder o costume</p>
+      
     </div>
   );
 }
---
+++ src/partials/Main/index.js
@@ -0,0 +1,24 @@
+import './styles.css'
+
+export default function AdvancedStatistics(){
+    return (
+        <main className="container-statistics">
+            <h1>Advanced Statistics</h1>
+            <p>Track how your links are performing across the web with our advanced statistics dashboard</p>
+            <ul>
+                <li>
+                    <div>
+                        Icon
+                    </div>
+                    <h3>
+                        Brand Recognition
+                    </h3>
+                    <p>
+                        Boost your brand recognition with each click. Generic links dont't mean a thing.
+                        Branded links help instil confindence in your content
+                    </p>
+                </li>
+            </ul>
+        </main>
+    )
+}
