54f18c9d6d752c2f1a6a55a8ae99a300bbae1083
luanmz <luanpereiramz@gmail.com>
2022-01-19 18:59:49 -0300

    refactor: modfiquei a utilização da imagem

--- src/Partials/Nav/index.js
+++ src/Partials/Nav/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import "./style.css"
+import logo from "../../assets/logo.svg"
 
 export default function Nav () {
     return(
         <nav className="navbar">
             <ul>
-                <li><a><img id="logo" src={require("../../assets/logo.svg")}/></a></li>
+                <li><a><img id="logo" src={logo}/></a></li>
                 <li><a>Features</a></li>
                 <li><a>Pricing</a></li>
                 <li><a>Resources</a></li>
