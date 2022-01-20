cb3ad374537f951dd60a4d24bd8b43bcc8101c21
Ayan Faustt <ayanfaustt@gmail.com>
2022-01-19 21:37:48 -0300

    juncao adv-statistics e footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css';
 import Footer from './partials/Footer';
+import AdvancedStatistics from './partials/Main'
 
 
 function App() {
   return (
+    <div>
+      <AdvancedStatistics></AdvancedStatistics>
       <Footer></Footer>
+    </div>
+      
   );
 }
 
