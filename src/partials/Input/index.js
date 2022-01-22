a294b30b4ec81171f31b6e3032c68d2f8b5a5826
Pedro Correia <pehcorreias@gmail.com>
2022-01-22 20:51:18 -0300

    alinhamento input

--- src/partials/Input/index.js
+++ src/partials/Input/index.js
@@ -1,74 +1,80 @@
 import React, { useState } from "react";
 import "./style.css";
 import Button from "../../components/Button";
-import axios from 'axios'
+import axios from "axios";
 
-function ShortLinkComponent(props){
-    return(
-        <div className="container-shortlink">
-            <span>{props.original}</span>
-            <div>
-                <a target='_blank' href={props.shorted}>{props.shorted}</a> 
-                <Button styled="br5 small">Copy</Button>
-            </div>
-        </div>
-    )
+function ShortLinkComponent(props) {
+  return (
+    <div className="container-shortlink">
+      <span>{props.original}</span>
+      <div>
+        <a target="_blank" href={props.shorted}>
+          {props.shorted}
+        </a>
+        <Button styled="br5 small">Copy</Button>
+      </div>
+    </div>
+  );
 }
 
 function Input() {
   const [stateInput, setStateInput] = useState("");
   const [active, setActive] = useState(false);
-  const [currentLinks, setCurrentLinks] = useState([])
+  const [currentLinks, setCurrentLinks] = useState([]);
 
+  function callAPI(url) {
+    axios
+      .get(`https://api.shrtco.de/v2/shorten?url=${url}`)
+      .then((response) => {
+        const { full_short_link: shortLink, original_link: originalLink } =
+          response.data.result;
 
-  function callAPI(url){
-    axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`).then(response=>{
-      const { full_short_link: shortLink, original_link: originalLink } = response.data.result
-      
-      setCurrentLinks([...currentLinks, {shortLink, originalLink}])
-    })
+        setCurrentLinks([...currentLinks, { shortLink, originalLink }]);
+      });
   }
 
-  
   return (
-      <div className="container-main">
-    <div className="design-entrada">
-      <div className="container-input">
-        <input
-          name="linkOriginal"
-          onChange={(e) => {
-            setStateInput(e.target.value);
+    <div className="container-main">
+      <div className="design-entrada">
+        <div className="container-input">
+          <input
+            name="linkOriginal"
+            onChange={(e) => {
+              setStateInput(e.target.value);
+            }}
+            type="text"
+            value={stateInput}
+            placeholder="Shorten a link here..."
+            className={(active ? "activeInput" : "") + " container-entrada"}
+          />
+          <span className={(active ? "active" : "") + " span-alert"}>
+            Please Add a link
+          </span>
+        </div>
+        <Button
+          styled="br5 big"
+          onClick={() => {
+            if (stateInput === "") {
+              setActive(true);
+            } else {
+              setActive(false);
+              setStateInput("");
+              callAPI(stateInput);
+            }
           }}
-          type="text"
-          value={stateInput}
-          placeholder="Shorten a link here..."
-          className={(active ? "activeInput" : "") + " container-entrada"}
-        />
-        <span className={(active ? "active" : "") + " span-alert"}>
-          Please Add a link
-        </span>
+        >
+          Shorten It!
+        </Button>
       </div>
-      <Button
-        styled="br5 big"
-        onClick={() => {
-          if (stateInput === "") {
-            setActive(true);
-          }else{
-              setActive(false)
-              setStateInput("")
-              callAPI(stateInput)
-          }
-        }}
-      >
-        Shorten It!
-      </Button>
-    </div>
-      { currentLinks.map((element, index)=>{
-          return (
-          <ShortLinkComponent key={index} original={element.originalLink} shorted={element.shortLink}></ShortLinkComponent>
-          )
-        })
-      }
+      {currentLinks.map((element, index) => {
+        return (
+          <ShortLinkComponent
+            key={index}
+            original={element.originalLink}
+            shorted={element.shortLink}
+          ></ShortLinkComponent>
+        );
+      })}
     </div>
   );
 }
--- src/partials/Nav/index.js
+++ src/partials/Nav/index.js
@@ -1,26 +1,36 @@
-import React from "react"
-import "./style.css"
-import logo from "../../assets/logo.svg"
-import Button from  "../../components/Button"
+import React from "react";
+import "./style.css";
+import logo from "../../assets/logo.svg";
+import Button from "../../components/Button";
 
-export default function Nav () {
-    return(
+export default function Nav() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a>
+            <img id="logo" src={logo} />
+          </a>
+        </li>
+        <li>
+          <a>Features</a>
+        </li>
+        <li>
+          <a>Pricing</a>
+        </li>
+        <li>
+          <a>Resources</a>
+        </li>
+      </ul>
 
-        <nav className="navbar">
-            <ul>
-                <li><a><img id="logo" src={logo}/></a></li>
-                <li><a>Features</a></li>
-                <li><a>Pricing</a></li>
-                <li><a>Resources</a></li>
-            </ul>
-
-            <ul>
-                <li><button id="login">Login</button></li>
-                <li><Button styled="br20 small">Sign up</Button></li>
-            </ul>
-            </nav>
-       
-    )
+      <ul>
+        <li>
+          <button id="login">Login</button>
+        </li>
+        <li>
+          <Button styled="br20 small">Sign up</Button>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
-
